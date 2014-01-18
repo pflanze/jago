a1d7466b0443d02459d6e66ce7a3b2e960eb3c2f
Christian Jaeger <chrjae@gmail.com>
2014-01-18 21:38:59 +0000

    clean play

--- main.scm
+++ main.scm
@@ -1,62 +1,83 @@
 (def (vector-every fn v)
      (every fn (vector->list v)))
 
-;; ------
+;; ------ Board -------
 
-(defenum allocation
-  white black empty)
+(defenum player
+  white black none)
 
 (def (board-vector? x)
      (and (vector? x)
 	  (= (vector-length x) 25)
-	  (vector-every allocation? x)))
+	  (vector-every player? x)))
 
 (defstruct board
   constructor-name: _board
   #(board-vector? fields))
 
 (def (make-board)
-     (_board (make-vector 25 'empty)))
+     (_board (make-vector 25 'none)))
 
 (def. (board.copy b)
      (_board (vector-copy (.fields b))))
 
-(def. (board.ref b x y)
-  (vector-ref (.fields b) (+ x (* 5 y))))
+(def. (board.ref b row col)
+  (vector-ref (.fields b) (+ col (* 5 row))))
 
-(def. (board.set! b x y #(allocation? v))
-  (vector-set! (.fields b) (+ x (* 5 y)) v))
+(def. (board.set! b row col #(player? v))
+  (vector-set! (.fields b) (+ col (* 5 row)) v))
 
-(def. (board.set b x y #(allocation? v))
+(def. (board.set b row col #(player? v))
   (let ((b* (.copy b)))
-    (board.set! b* x y v)
+    (board.set! b* row col v)
     b*))
 
-(def. (board.row b i)
-  (map (L (j)
-	  (.ref b j i))
+(def. (board.row b row)
+  (map (L (col)
+	  (.ref b row col))
        (iota 5)))
 
 (def. (board.show b)
-  (map (L (i)
-	  (.row b i))
+  (map (L (row)
+	  (.row b row))
        (iota 5)))
 
+
+
+;; ------- Round -------
+
+(defstruct round
+  #(board? board)
+  #(player? next-player))
+
+(def (other-player p)
+     (xcase p
+	    ((white) 'black)
+	    ((black) 'white)))
+
+(def. (round.play r row col)
+     (round (.set (.board r) row col (.next-player r))
+	    (other-player (.next-player r)) ))
+
+;; input / output
+
+
 ;; "1 4" ~> (let x 1)(let y 4)
-(def (f str fn)
+(def (parse-input str fn)
      (let ((l (with-input-from-string str read-all)))
        (fn (car l) (cadr l))))
 
-(def (go-loop)
+(def (start-go)
      (read-line)
      (letrec ((loop
-	       (L (board)
-		  (println "Place your stone (y x)")
+	       (L (r)
+		  (println (.next-player r) ", place your stone (row col)")
 		  (let* ((line (read-line))
-			 (board* (f line (L (y x)
-					    (board.set board (dec x) (dec y) 'white)) )))
+			 (r* (parse-input line
+					  (L (row col)
+					     (.play r (dec row) (dec col))  ) )))
 		    (println "New board:")
-		    (pretty-print (.show board*))
-		    (loop board*)))))
-       (loop (make-board))))
+		    (pretty-print (.show (.board r*)))
+		    (loop r*)))))
+       (loop (round (make-board) 'white))))
 

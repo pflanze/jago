f27ea639d408bb8ea6f62ca2569fe2774a14130f
Christian Jaeger <chrjae@gmail.com>
2014-01-27 01:59:52 +0000

    add |->|

--- easy.scm
+++ easy.scm
@@ -50,3 +50,16 @@
  #t
  )
 
+(define-macro* (-> pred expr)
+  (with-gensym V
+	       `(let ((,V ,expr))
+		  (if (,pred ,V) ,V
+		      (error "value fails to meet predicate:" ,V)))))
+
+(TEST
+ > (-> number? 5)
+ 5
+ > (%try-error (-> number? "5"))
+ #(error "value fails to meet predicate:" "5")
+ )
+

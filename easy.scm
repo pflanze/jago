85bd9a7b17c1e6f62b465f3f0ccb331dce4b5878
Christian Jaeger <chrjae@gmail.com>
2014-01-27 01:54:56 +0000

    add |values-of|

--- easy.scm
+++ easy.scm
@@ -20,3 +20,33 @@
 (def (vector-every fn v)
      (every fn (vector->list v)))
 
+(def (values-of . preds)
+     (let ((len (length preds)))
+       (if (= len 1)
+	   (car preds)
+	   (L (v)
+	      (and (values? v)
+		   (let ((vals (values->list v)))
+		     (and (= (length vals) len)
+			  (every (L (val pred)
+				    (pred val))
+				 vals
+				 preds))))))))
+
+(TEST
+ > ((values-of boolean? string?) (values #f ""))
+ #t
+ > ((values-of boolean? string?) (values #f))
+ #f
+ > ((values-of boolean?) (values #f))
+ #t
+ > ((values-of) (values))
+ #t
+ > ((values-of) (values 1 2))
+ #f
+ > ((values-of integer? number?) (values 1.4 2))
+ #f
+ > ((values-of integer? number?) (values 2 1.4))
+ #t
+ )
+
